9a134b7700b97874ee66390744f967897178d1f5
Ibigoni Inye-Tam <inyetamibigoni@gmail.com>
2025-06-14 11:59:37 -0500

    Added PropTypes safety and stored min/max value in a const

--- src/components/AlgorithmControls.jsx
+++ src/components/AlgorithmControls.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function AlgorithmControls({ 
   algorithm, 
@@ -12,6 +13,11 @@ function AlgorithmControls({
   onReset,
   isRunning 
 }) {
+  const minArrSize = 10;
+  const maxArrSize = 100;
+  const minSpeed = 1;
+  const maxSpeed = 10; 
+  
   return (
     <div className="controls-panel">
       <div className="control-group">
@@ -40,8 +46,8 @@ function AlgorithmControls({
         <input 
           type="range" 
           id="arraySize" 
-          min="10" 
-          max="100" 
+          min={minArrSize} 
+          max={maxArrSize} 
           value={arraySize} 
           onChange={onSizeChange}
           disabled={isRunning}
@@ -54,8 +60,8 @@ function AlgorithmControls({
         <input 
           type="range" 
           id="speed" 
-          min="1" 
-          max="100" 
+          min={minSpeed} 
+          max={maxSpeed}
           value={speed} 
           onChange={onSpeedChange}
         />
@@ -89,4 +95,16 @@ function AlgorithmControls({
   );
 }
 
+AlgorithmControls.propTypes = {
+  algorithm: PropTypes.string.isRequired,
+  onAlgorithmChange: PropTypes.func.isRequired,
+  arraySize: PropTypes.number.isRequired,
+  onSizeChange: PropTypes.func.isRequired,
+  speed: PropTypes.number.isRequired,
+  onSpeedChange: PropTypes.func.isRequired,
+  onStart: PropTypes.func.isRequired,
+  onStop: PropTypes.func.isRequired,
+  onReset: PropTypes.func.isRequired,
+  isRunning: PropTypes.bool.isRequired
+} 
 export default AlgorithmControls;
