6738ab3503d12ccf9d657ade061a5b611d7ffb4a
Ibigoni Inye-Tam <inyetamibigoni@gmail.com>
2025-06-16 13:25:07 -0500

    update tailwind imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './styles/App.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
